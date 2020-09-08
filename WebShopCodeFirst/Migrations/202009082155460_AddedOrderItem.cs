aacd356adbf2e82c0c38b1317fb35a0272e803c4
Nemanja Nikolć <58153223+nexencode@users.noreply.github.com>
2020-09-09 00:04:51 +0200

    Added Migrations.txt

---
+++ WebShopCodeFirst/Migrations/202009082155460_AddedOrderItem.cs
@@ -0,0 +1,30 @@
+﻿namespace WebShopCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedOrderItem : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        OrderItemId = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderItemId)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "ProductId", "dbo.Products");
+            DropIndex("dbo.OrderItems", new[] { "ProductId" });
+            DropTable("dbo.OrderItems");
+        }
+    }
+}
